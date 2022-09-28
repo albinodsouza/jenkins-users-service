7a9db88befceccd79f3ec5d0a608c62bec384d1d
Albino <albinodsouza151@gmail.com>
2022-09-28 12:30:57 +0530

    second commit

---
+++ src/main/java/com/sony/service/UserManager.java
@@ -0,0 +1,37 @@
+package com.sony.service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.sony.model.User;
+
+@Service
+public class UserManager {
+	private Map<String, User> users;
+	
+	public UserManager() {
+	users = new HashMap<>();
+	User user;
+	
+	user = new User("Albino","Dsouza","some@gmail.com","Kaiga");
+	users.put(user.getId(), user);
+	
+	user = new User("john","doe","john@gmail.com","texas");
+	users.put(user.getId(), user);
+	
+	user = new User("jack","sparrow","jack@gmail.com","NY");
+	users.put(user.getId(), user);
+}
+
+public Iterable<User>getAllUsers(){
+	return this.users.values();
+}
+public User getUserById(String id) {
+	return this.users.get(id);
+}
+	
+	
+
+}
